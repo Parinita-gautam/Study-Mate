c022ef6f7fb97f0f3abf94e772b8ee20a05efc81
Parinita-gautam <parinitagautam26@gmail.com>
2026-06-16 15:59:57 +0545

    added chat-widget

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -6,6 +6,10 @@ import Dashboard from "./Dashboard"; // this contains sections with ids: home, c
 export default function LandingPage() {
   return (
     <>
+    <script
+        src="https://staging.chatboq.com/widget.js"
+        data-orgId="114b5d0b-5dce-4b1c-ac5a-82ed3eb9d369"
+      ></script>
       <LandingNavbar />
       <Dashboard />
     </>
