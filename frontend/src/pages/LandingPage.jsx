3a59444b083bfba9988b33cae1db6409bcd05017
Parinita-gautam <parinitagautam26@gmail.com>
2026-06-16 18:44:22 +0545

    chat widget added, ready for production

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,15 +1,29 @@
 // src/pages/LandingPage.jsx
-import React from "react";
+import React,{useEffect} from "react";
 import LandingNavbar from "../components/common/LandingNavbar";
 import Dashboard from "./Dashboard"; // this contains sections with ids: home, courses, faqs, contact
 
 export default function LandingPage() {
+   useEffect(() => {
+    // prevent duplicate script injection
+    const existingScript = document.querySelector(
+      'script[src="https://staging.chatboq.com/widget.js"]'
+    );
+
+    if (!existingScript) {
+      const script = document.createElement("script");
+      script.src = "https://staging.chatboq.com/widget.js";
+      script.async = true;
+      script.setAttribute(
+        "data-orgId",
+        "114b5d0b-5dce-4b1c-ac5a-82ed3eb9d369"
+      );
+
+      document.body.appendChild(script);
+    }
+  }, []);
   return (
     <>
-    <script
-        src="https://staging.chatboq.com/widget.js"
-        data-orgId="114b5d0b-5dce-4b1c-ac5a-82ed3eb9d369"
-      ></script>
       <LandingNavbar />
       <Dashboard />
     </>
